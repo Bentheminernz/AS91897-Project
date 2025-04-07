54c68681ceb7197de25223e9b9efc5cdaa6bfb46
Bentheminernz <65065992+Bentheminernz@users.noreply.github.com>
2025-04-07 12:39:50 +1200

    Refactor QuestionBlock class to improve rendering and collision detection

--- Components/QuestionBlock.py
+++ Components/QuestionBlock.py
@@ -3,24 +3,22 @@ import pygame
 class QuestionBlock(pygame.sprite.Sprite):
     def __init__(self, image_path, position, text):
         super().__init__()
-        self.image = pygame.image.load(image_path)
-        self.image = pygame.transform.scale(self.image, (100, 100))
-        self.rect = self.image.get_rect(topleft=position)
+        self.original_image = pygame.image.load(image_path)
+        self.original_image = pygame.transform.scale(self.original_image, (100, 100))
         self.text = str(text)
+        self.is_killed = False
 
-
-    def update(self, *args):
-        # draw the question block
-        screen = args[0]
-        screen.blit(self.image, self.rect)
-        font = pygame.font.Font(None, 100)
+        self.image = self.original_image.copy()
+        font = pygame.font.Font(None, 18)
         text_surface = font.render(self.text, True, (255, 255, 255))
-        text_rect = text_surface.get_rect(center=self.rect.center)
-        screen.blit(text_surface, text_rect)
-        print(f"Renderinf  text: {self.text}")
+        text_rect = text_surface.get_rect(center=(50, 50))
+        self.image.blit(text_surface, text_rect)
+        
+        self.rect = self.image.get_rect(topleft=position)
 
     def on_collision(self, player):
         # this detects if the player collides with the question block
-        if self.rect.colliderect(player.rect):
+        if self.rect.colliderect(player.rect) and not self.is_killed:
             print("Collision detected!")
+            self.is_killed = True
             self.kill()
