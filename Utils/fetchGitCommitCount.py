e5fe847bb41aa1e4f6da42f2deaa522a1f9e55f6
Bentheminernz <65065992+Bentheminernz@users.noreply.github.com>
2025-04-10 14:54:42 +1200

    Add dependencies and update question block behavior

--- Utils/fetchGitCommitCount.py
+++ Utils/fetchGitCommitCount.py
@@ -1,10 +1,14 @@
 import subprocess
 
+
 def fetch_git_commit_count():
     subprocess.run(["git", "rev-list", "--count", "HEAD"], check=True)
-    commit_count = subprocess.check_output(["git", "rev-list", "--count", "HEAD"]).strip()
-    count = int(commit_count.decode('utf-8'))
+    commit_count = subprocess.check_output(
+        ["git", "rev-list", "--count", "HEAD"]
+    ).strip()
+    count = int(commit_count.decode("utf-8"))
     return count
+
 
 if __name__ == "__main__":
     fetch_git_commit_count()
